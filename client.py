b4c09cef5a8fdaf3c6a15d742d7ce0946d16de2d
Josh Ferrero <joshferrero123@gmail.com>
2019-06-08 18:07:52 -0400

    Added client/server files. Added basic socket functionality and ADD and LIST functionality

---
+++ client.py
@@ -0,0 +1,63 @@
+import _thread as thread
+from socket import *
+
+SERVER_PORT = 7734
+
+def send_request(request, server_host, server_port):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.connect((server_host, server_port))
+    sock.send(request.encode())
+    response = sock.recv(1024).decode()
+    print('\r\n' + response)
+    sock.close()
+
+def join(client_host, client_port, server_host, server_port):
+    request = "JOIN P2P-CI/1.0\r\nHost: %s\r\nPort: %s" % (client_host, client_port)
+    send_request(request, server_host, server_port)
+
+def add_rfc(client_host, client_port, server_host, server_port):
+    print('Enter an RFC Number: ')
+    rfc_number = int(input())
+    print('Enter an RFC title: ')
+    rfc_title = input()
+    request = "ADD RFC %d P2P-CI/1.0\r\nHost: %s\r\nPort: %d\r\nTitle: %s\r\n" % (rfc_number, client_host,
+            client_port, rfc_title)
+    send_request(request, server_host, server_port)
+
+def list_rfcs(client_host, client_port, server_host, server_port):
+    print('Enter a phrase to search for or ALL to list all RFCs: ')
+    phrase = input()
+    request = "LIST %s P2P-CI/1.0\r\nHost: %s\r\nPort: %d" % (phrase.upper(), client_host, client_port)
+    send_request(request, server_host, server_port)
+
+def main():
+    client_host = input('Enter a unique hostname: ')
+    client_port = int(input('Enter a unique port number: '))
+
+    server_host = input('Enter hostname of central server: ')
+    server_port = SERVER_PORT
+
+    join(client_host, client_port, server_host, server_port)
+    while True:
+        print('Enter the command you would like to run (ADD, GET, LIST, LOOKUP, QUIT): ')
+        user_input = input()
+        if user_input.lower() == 'add':
+            add_rfc(client_host, client_port, server_host, server_port)
+        elif user_input.lower() == 'get':
+            print('not implemented')
+            exit(1)
+        elif user_input.lower() == 'list':
+            list_rfcs(client_host, client_port, server_host, server_port)
+            exit(1)
+        elif user_input.lower() == 'lookup':
+            print('not implemented')
+            exit(1)
+        elif user_input.lower() == 'quit':
+            print('Exiting.')
+            exit(0)
+        else:
+            print('Invalid command. Please try again.\r\n')
+            continue
+        
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,91 @@
+from socket import *
+
+SERVER_PORT = 7734
+
+rfc_index = []
+peers = []
+
+class Peer:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+class RFC:
+    def __init__(self, number, title, host, port):
+        self.number = number
+        self.title = title
+        self.host = host
+        self.port = port
+
+def join(request, client_socket):
+    # Request format:
+    # JOIN P2P-CI/1.0
+    # Host: HELLO
+    # Port: 12000
+    host = request[1].split(':')[1]
+    port = request[2].split(':')[1]
+    curr_peer = Peer(host, port)
+    peers.append(curr_peer)
+    client_socket.send('Successfully connected to host.\r\n'.encode())
+
+def add(request, client_socket):
+    # Request format:
+    # ADD RFC 123 P2P-CI/1.0
+    # Host: thishost.csc.ncsu.edu
+    # Port: 5678
+    # Title: A Proferred Official ICP 
+    rfc_number = request[0].split(' ')[2]
+    rfc_title = request[3].split(':')[1]
+    host = request[1].split(':')[1]
+    port = request[2].split(':')[1]
+    rfc = RFC(rfc_number, rfc_title, host, port)
+    rfc_index.append(rfc)
+    message = "P2P-CI/1.0 200 OK\r\nRFC%s %s %s %s\r\n" % (rfc_number, rfc_title, host, port)
+    client_socket.send(message.encode())
+
+def list_rfcs(request, client_socket):
+    # Request format:
+    # LIST ALL P2P-CI/1.0 
+    # Host: thishost.csc.ncsu.edu 
+    # Port: 5678
+    host = request[1].split(':')[1]
+    port = request[2].split(':')[1]
+    phrase = request[0].split(' ')[1]
+    if phrase.lower() == 'all':
+        phrase = ''
+
+    message = ''
+    message += 'P2P-CI/1.0 200 OK %s\r\n' % phrase
+    for rfc in rfc_index:
+        if phrase.lower() in rfc.title.lower():
+            message += 'RFC%s %s %s %s\r\n' % (rfc.number, rfc.title, host, port)
+        else:
+            message += '%s\r\n%s\r\n' % (phrase, rfc.title)
+    client_socket.send(message.encode())
+
+def main():
+    server_port = SERVER_PORT
+    server_sock = socket(AF_INET, SOCK_STREAM)
+    server_sock.bind(('', server_port))
+    server_sock.listen(1)
+    print('Awaiting connection.\r\n')
+    while True:
+        client_sock, client_addr = server_sock.accept()
+        request = client_sock.recv(1024).decode()
+        print(request + "\r\n")
+        req_list = request.split('\r\n')
+        if "JOIN" in req_list[0]:
+            join(req_list, client_sock)
+        elif "ADD" in req_list[0]:
+            add(req_list, client_sock)
+        elif "LIST" in req_list[0]:
+            list_rfcs(req_list, client_sock)
+        else:
+            error_msg = "P2P-CI/1.0 400 Bad Request\r\n"
+            client_sock.send(error_msg.encode())
+        client_sock.close()
+    print('Exiting.\r\n')
+    server_sock.close()
+
+if __name__ == '__main__':
+    main()
